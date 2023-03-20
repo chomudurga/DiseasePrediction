fbbefc272fc436e851b79a426552a65834479229
chomudurga <127102305+chomudurga@users.noreply.github.com>
2023-03-20 14:19:21 +0530

    Update index.js

--- router/symptoms/index.js
+++ router/symptoms/index.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const { insertsymptom } = require('./insertion');
+const {getbyid, updatesymptom} = require('./updation');
+const {list} =require("./list");
 
+const router = express.Router();
+
+router.post('/insert',insertsymptom);
+router.get('/getbyid',getbyid);
+router.put('/updatesymptom',updatesymptom);
+router.get('/list',list);
+
+
+module.exports =  router ;
