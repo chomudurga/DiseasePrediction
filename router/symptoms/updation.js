61189f86ce5ba5e74ac40ce7ad656e9b933635de
chomudurga <127102305+chomudurga@users.noreply.github.com>
2023-03-20 14:17:17 +0530

    Update and rename symptomUpdation.js to updation.js

---
+++ router/symptoms/updation.js
@@ -0,0 +1,28 @@
+const body = require('body-parser');
+const Symptom = require('../../models/symptoms.js');
+
+
+const getbyid = async (req,res) =>{
+    const symptomid = req.body.symptomid;
+    let symp = await Symptom.findByPk(symptomid);
+    return res.status(200).send(symp);
+};
+
+const updatesymptom = (req,res) => {
+    const symptomid = req.body.symptomid;
+
+    Symptom.findByPk(symptomid)
+    .then((symptom) => {
+         symptom.symptom_name = req.body.symptomname;
+         return symptom.save();
+    })
+   .then(() =>{
+    console.log("updated");
+   })
+   .catch((error) => {
+    console.log(error);
+   })
+}
+
+module.exports = { getbyid ,
+    updatesymptom }
