e1f9e70b5aa7157a8e8dc07f164ca49fe318354f
chomudurga <127102305+chomudurga@users.noreply.github.com>
2023-03-20 14:17:33 +0530

    Update index.js

--- router/index.js
+++ router/index.js
@@ -1,1 +1,12 @@
+import express from 'express'
+import disease from './diseases/index.js';
+import symptom from './symptoms/index.js';
 
+const app =express()
+//app.use(express.json)
+
+app.use('/disease',disease);
+app.use('/symptom',symptom);
+
+
+export default app
