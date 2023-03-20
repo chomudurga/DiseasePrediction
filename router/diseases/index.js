9f37871d973fe54d92b3ec0f4ae5a0f764fa54ae
chomudurga <127102305+chomudurga@users.noreply.github.com>
2023-03-20 17:06:32 +0530

    Update index.js

--- router/diseases/index.js
+++ router/diseases/index.js
@@ -1,14 +1,14 @@
 import express from 'express'
 
 import diseaseinsert from './insertion.js'
-// import update from './updation.js'
-// import deleteDisease from './deletion.js'
-// import list from './list.js'
+import update from './updation.js'
+import deleteDisease from './deletion.js'
+import list from './list.js'
 const router = express.Router()
 
 router.post('/add',diseaseinsert);
-// router.put('/update',update);
-// router.delete('/delete', deleteDisease);
-// router.get('/list',list);
+router.put('/update',update);
+router.delete('/delete', deleteDisease);
+router.get('/list',list);
 
 export default router
