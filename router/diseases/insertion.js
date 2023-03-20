b95c35fe01aa0068712a342c0ca838870a11588c
chomudurga <127102305+chomudurga@users.noreply.github.com>
2023-03-20 17:05:23 +0530

    Update insertion.js

--- router/diseases/insertion.js
+++ router/diseases/insertion.js
@@ -19,24 +19,3 @@ const diseaseinsert = (request,response)=>{
     }
 export default diseaseinsert
 
-// import body from 'body-parser'
-
-// import diseases from '../../model/diseases.js'
-
-// import insertDisease from './index.js'
-
-// const insertDisease = (request,response)=>{
-//     const insert ={
-//         diseaseName : request.body.diseaseName
-//     }
-//     diseases.create(insert)
-//     .then(()=>{
-//         console.log(request.body)
-//     })
-//     .catch((error)=>{
-//           console.log(error)
-//     })
-//     return response.send()
-// }
-
-// export default insertDisease
